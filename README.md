843143951ad35b4eae38fc111c6a7a043eb806f5
liukeweiaway <Liukeweiaway@hotmail.com>
2020-05-03 16:39:40 +0800

    increase information and optimize

---
+++ README.md
@@ -0,0 +1,46 @@
+im6A-TS-cnn
+====
+Description:
+------------
+im6A-TS-cnn is able to identify multi-tissues in Human, Mouse and Rat.
+
+Installation:
+-------------
+- <span  style="color: #5bdaed; font-weight: bold">python3.6</span>
+- pandas==1.0.3
+- tensorflow-cpu==2.1.0
+- numpy==1.18.3
+``` 
+pip install pandas==1.0.3 tensorflow-cpu==2.1.0 numpy==1.18.3
+``` 
+``` 
+pip install -r yours/requirement.txt
+``` 
+Optional arguments:
+-------------------
+```
+  -h, --help            show this help message and exit
+  --addresses ADDRESSES
+                        Liukeweiaway@hotmail.com
+  -i INPUTFILE, --inputFile INPUTFILE
+                        -i input.txt (The input file is a complete Fasta
+                        format sequence.)
+  -o OUTPUTFILE, --outputFile OUTPUTFILE
+                        -o output.html (Results are saved under results
+                        folder.)
+  -s SPECIES, --species SPECIES
+                        -m Human/Mouse/Rat (Choose one from three species to
+                        use.)
+  -m MODIFICATION, --tissues TISSUES
+                        brain/liver/kidney/heart/testis (We can choose testis you want)
+```
+Example:
+--------
+```
+python im6A-TS-cnn.py -i sequence.txt -o ccc.html -s Human -t brain
+```
+***
+Version number：V0.0.1 <br>
+Updated date：2020-05-03 <br>
+Email: Liukeweiaway@hotmai.com 
+***
---
+++ im6A-TS-cnn.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8-*-
+# author: Liukewei time:2020/1/21 QQ:422209303 e-mail:Liukeweiaway@hotmail.com
+# ----------------------------------------------------------------------------
+import argparse
+from mainf import preproces
+import warnings
+from mainf.formatCheck import formatCheck
+warnings.filterwarnings("ignore")
+
+
+def parse_args():
+    """
+    :return:进行参数的解析
+    """
+    description = "iMRMF is able to simultaneously identify m6A, m5C, m1A, ψ and A-to-I modifications in Homo sapiens, Mus musculus and Saccharomyces cerevisiae. Thank you very much for submitting the error message to Liukeweiaway@hotmail.com.  Example: python im6A-TS-cnn.py -i sequence.txt -o ccc.html -s Human -m all -t normal "
+    parser = argparse.ArgumentParser(description=description)
+    help = "The correct path of address"
+    parser.add_argument('--addresses', help=help)
+    parser.add_argument('-i', '--inputFile', help='-i input.txt (The input file is a complete Fasta format sequence.)')
+    parser.add_argument('-o', '--outputFile', help='-o output.html (Results are saved under results folder.)')
+    parser.add_argument('-s', '--species', help='-s Human/Mouse/Rat (Choose one from three species to use.)')
+    parser.add_argument('-t', '--tissues', help='-t brain/liver/kidney/heart/testis (We can choose testis you want.)')
+    # parser.add_argument('-t', '--threshold', help='-m low/normal/high (We offer 3 options based on the difference in specificity, which are low, normal and high.)')
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    formatCheck = formatCheck(args.inputFile, args.outputFile, args.species, args.tissues)
+    print(formatCheck)
+    preproces.preprocesser(args.inputFile, args.outputFile, args.species, args.tissues)
+    # previous_deal(args.inputFile, args.outputFile, args.featureList)  # feature extraction
